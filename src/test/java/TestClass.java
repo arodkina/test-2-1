cabf0b2035a4e5098dc81892bd643138a58577c3
arodkina <arodkina@devexperts.com>
2021-07-29 21:31:54 +0300

    5

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,22 @@
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestClass {
+    @Test
+    void shouldTest() {
+        open("http://localhost:9999");
+        SelenideElement form = $(By.className("App_appContainer__3jRx1"));
+        form.$("[data-test-id=name] input").setValue("Василий");
+        form.$("[data-test-id=phone] input").setValue("+79279879076");
+        form.$("[data-test-id=agreement]").click();
+        form.$(By.className("button__text")).click();
+        $(By.xpath("//div[@id='root']/div[@class='App_appContainer__3jRx1']//p[@class='paragraph paragraph_theme_alfa-on-white']")).shouldBe(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
+
+
