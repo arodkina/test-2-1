d76192f1e285477803f1e615cfcecc08a0f95240
arodkina <arodkina@devexperts.com>
2021-07-31 00:36:36 +0300

    8

--- src/test/java/TestSeleniumClass.java
+++ src/test/java/TestSeleniumClass.java
@@ -42,4 +42,17 @@ public class TestSeleniumClass {
         String text = driver.findElement(By.xpath("//div[@id='root']/div[@class='App_appContainer__3jRx1']//p[@class='paragraph paragraph_theme_alfa-on-white']")).getText();
         assertEquals ("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
+
+    @Test
+    public void wrongPhoneSelenium() throws InterruptedException {
+        driver.get("http://localhost:9999");
+        Thread.sleep(500);
+        WebElement form = driver.findElement(By.xpath("//div[@id='root']/div[@class='App_appContainer__3jRx1']"));
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василий");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+7952");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button__text")).click();
+        String text = driver.findElement(By.xpath("//div[@id='root']/div[@class='App_appContainer__3jRx1']/form[@action='/']/div[2]/span/span[@class='input__inner']/span[@class='input__sub']")).getText();
+        assertEquals ("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+    }
 }
