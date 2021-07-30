92212104f31f25cb61283df661bfc22ef98831c8
arodkina <arodkina@devexperts.com>
2021-07-30 03:42:28 +0300

    5

---
+++ src/test/java/TestClassSelenide.java
@@ -0,0 +1,33 @@
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestClassSelenide {
+    @Test
+    void shouldTest() {
+        open("http://localhost:9999");
+        SelenideElement form = $(By.className("App_appContainer__3jRx1"));
+        form.$("[data-test-id=name] input").setValue("Василий");
+        form.$("[data-test-id=phone] input").setValue("+79279879076");
+        form.$("[data-test-id=agreement]").click();
+        form.$(By.className("button__text")).click();
+        $(By.xpath("//div[@id='root']/div[@class='App_appContainer__3jRx1']//p[@class='paragraph paragraph_theme_alfa-on-white']")).shouldBe(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void wrongPhone() {
+        open("http://localhost:9999");
+        SelenideElement form = $(By.className("App_appContainer__3jRx1"));
+        form.$("[data-test-id=name] input").setValue("Василий");
+        form.$("[data-test-id=phone] input").setValue("+7927");
+        form.$("[data-test-id=agreement]").click();
+        form.$(By.className("button__text")).click();
+        $(By.xpath("//div[@id='root']//form[@action='/']/div[2]/span//span[@class='input__sub']")).shouldBe(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+}
+
+
---
+++ src/test/java/TestSeleniumClass.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestSeleniumClass {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll(){
+        System.setProperty("webdriver.chrome.driver","/driver/win/chromedriver");
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown(){
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    public void shouldTestBe(){
+        driver.get("http://localhost:9999");
+        driver.findElement(By.xpath("//div[@id='root']//form[@action='/']/div[1]/span//span[@class='input__box']")).sendKeys("Василий");
+        driver.findElement(By.xpath("//div[@id='root']//form[@action='/']/div[2]/span")).sendKeys("+79522865643");
+        driver.findElement(By.xpath("//div[@id='root']//form[@action='/']//label//input[@name='agreement']")).click();
+        driver.findElement(By.xpath("//div[@id='root']//form[@action='/']//button[@role='button']//span[@class='button__text']")).click();
+        String text = driver.findElement(By.xpath("/html//div[@id='root']//p[@class='paragraph paragraph_theme_alfa-on-white']")).getText();
+        assertEquals ("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+    private void assertEquals(String s, String trim) {
+    }
+}
